16a00622262f2b440bd215b59607cd3f0b7d5f74
Sulaiman <sulaimansamad14@gmail.com>
2024-12-06 21:56:03 +0500

    Update Classification Models (Machine Learning).py
    This is an implementation of machine learning models to classify data. It includes the statistic models KNN, Naive Bayes, Logistic Regression and a Support Vector Classifier (SVC).
    
    Link for the data = https://archive.ics.uci.edu/dataset/159/magic+gamma+telescope

--- Classification Models (Machine Learning).py
+++ Classification Models (Machine Learning).py
@@ -17,15 +17,15 @@ df = pd.read_csv('/Users/Suli/Documents/Python Practice/Datasets/magic+gamma+tel
 # Binary encoding the class column to make it true or false. 1 is true, 0 is false
 df['class'] = (df['class'] == 'g').astype(int)
 
-# Making histograms for each column in the dataframe, where one is gamma and one is hadron for each column.
-# for label in cols[:-1]:
-#     plt.hist(df[df['class']==1][label], color = 'blue', label = 'gamma', alpha = 0.7, density=True)
-#     plt.hist(df[df['class'] ==0][label], color = 'red', label = 'hadron', alpha = 0.7, density=True)
-#     plt.title(label)
-#     plt.ylabel('Probability')
-#     plt.xlabel(label)
-#     plt.legend()
-#     plt.show()
+Making histograms for each column in the dataframe, where one is gamma and one is hadron for each column.
+for label in cols[:-1]:
+    plt.hist(df[df['class']==1][label], color = 'blue', label = 'gamma', alpha = 0.7, density=True)
+    plt.hist(df[df['class'] ==0][label], color = 'red', label = 'hadron', alpha = 0.7, density=True)
+    plt.title(label)
+    plt.ylabel('Probability')
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
 
 # Train, validation and test datasets.
 
